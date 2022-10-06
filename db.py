e80c953145c68f79e16f0c31d3ebd1029bc514dd
thimant <36861589+thimant@users.noreply.github.com>
2022-10-06 18:00:17 -0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,20 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<USERNAME>:<PASSWORD>@<RDSENDPOINT>/<DBNAME>?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Senha_10@database-app-demo.cpei6fi7fzmj.us-west-2.rds.amazonaws.com/countpool?charset=utf8mb4'
+
+db = SQLAlchemy(app)
+
+class students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))  
+   addr = db.Column(db.String(200))
+   pin = db.Column(db.String(10))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
